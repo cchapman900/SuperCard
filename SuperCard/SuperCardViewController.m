10c7691ca475f1a95fa04364d6f37a83c31f7a53
Chris Chapman <chrischapman900@gmail.com>
2013-05-28 02:15:43 -0400

    Initial Commit

---
+++ SuperCard/SuperCardViewController.m
@@ -0,0 +1,29 @@
+//
+//  SuperCardViewController.m
+//  SuperCard
+//
+//  Created by Chris on 5/28/13.
+//  Copyright (c) 2013 Chris Chapman. All rights reserved.
+//
+
+#import "SuperCardViewController.h"
+
+@interface SuperCardViewController ()
+
+@end
+
+@implementation SuperCardViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SuperCard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SuperCard
+//
+//  Created by Chris on 5/28/13.
+//  Copyright (c) 2013 Chris Chapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SuperCardAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SuperCardAppDelegate class]));
+    }
+}
