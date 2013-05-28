a497eb6b7edd96d47425605559bf9bb5fb3722af
Chris Chapman <chrischapman900@gmail.com>
2013-05-28 02:18:49 -0400

    Initial commit

---
+++ SuperCard/PlayingCardView.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardView.h
+//  SuperCard
+//
+//  Created by Chris on 5/28/13.
+//  Copyright (c) 2013 Chris Chapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayingCardView : UIView
+
+@end
